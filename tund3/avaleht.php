4ed751a4b45e09d0cdede1b1173157ec52cb0c9b
Silver Tamm <silvertamm@users.noreply.github.com>
2017-09-18 15:46:17 +0300

    uuendatud

---
+++ tund3/avaleht.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Silver Tamme Mängukoobas
+	</title>
+</head>
+<body>
+	
+	<p>Siin veebilehel pole teil midagi teha, kuna see veebileht on loodud õppetöö jaoks</p>
+	<h2>Mõned lingid</h2>
+	<p>Õpime <a href="http://www.tlu.ee">Tallinna Ülikoolis</a>.</p>
+	<p>Minu esimene php-leht on <a href="../esimene.php">siin</a>.</p>
+	<p>Kuna mul sõpru pole, siis on minu enda leht <a href="../../../../~tammsilv/veebiprogrammeerimine">siin</a>.</p>
+	<p>Pilte ülikoolist näeb <a href="foto.php">siin</a>.</p>	
+</body>
+</html>
---
+++ tund3/foto.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Silver Tamme Mängukoobas
+	</title>
+</head>
+
+<body>
+	<p>Siin veebilehel pole teil midagi teha, kuna see veebileht on loodud õppetöö jaoks</p>
+	<h2>Pilt ülikoolist</h2>
+	<img src=../../pics/tlu3.jpg" alt="Tallinna Ülikool">
+	
+
+
+
+</body>
+</html>
